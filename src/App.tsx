5dad5f9ed6376e6edf077af81102eeab769800d6
LetsWebDevelop <maxphoogland@gmail.com>
2022-02-27 09:57:27 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BreadCrumbs, Card, Divider } from "@letswebdevelop/lwdds";
+import "./App.css";
 
 function App() {
+  const cardContent = [
+    { title: "LetsWebDevelop", body: "Well Hello World - Speaking to you from LWDDS Card" },
+    { title: "LWDDS", body: "Pronounced as Lewds" },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <p>HELLO</p>
+      </div>
+
+      <BreadCrumbs
+        currentPage="current page"
+        currentPageColor="green"
+        dividerColor="hsla(0, 56%, 3%, 0.35)"
+        homePreviousPageColor="grey"
+        hoverColor="blue"
+        link={["Previous Page", "Previous Page 2"]}
+      />
+      <Divider size="xl" dividerColor="var(--color-primary)" thick={false} />
+      <div style={{ marginTop: 25, display: "flex", flexDirection: "row", justifyContent: "space-around" }}>
+        {cardContent.map((content) => {
+          return (
+            <div key={content.title}>
+              <Card
+                label={content.title}
+                size="medium"
+                textAlignBody="left"
+                body={content.body}
+                border
+                borderColor="wheat"
+                borderRadius="5%"
+              />
+            </div>
+          );
+        })}
+      </div>
+      <Divider style={{ marginTop: 25 }} dividerColor="#a808f3" fade={true} size="l" thick />
+
+      <div style={{ display: "flex", justifyContent: "center", marginTop: 25 }}>
+        <Card
+          backgroundColor="#3d3939"
+          backgroundTitleColor="#3d3939"
+          body="Dr. Dre"
+          border
+          borderColor="gold"
+          borderRadius="1rem"
+          color="#2aa200"
+          image="https://oor.nl/media/2021/01/dr-dre-press-1024x683.jpg"
+          label="The Chronic"
+          size="large"
+        />
+      </div>
     </div>
   );
 }
